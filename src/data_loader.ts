0f9983907a2152e72a9131c3dc23f2a579561732
Alex Carrithers <38507729+afenrir@users.noreply.github.com>
2019-06-11 12:08:29 +0100

    Specify fetch parameter type.

--- src/data_loader.ts
+++ src/data_loader.ts
@@ -7,7 +7,7 @@ import Factom from 'factom';
 export class FactomdDataLoader {
     constructor(private cli: Factom.FactomCli) {}
 
-    private createDataloader<K, V>(fetch: Function) {
+    private createDataLoader<K, V>(fetch: (key: K) => Promise<V>) {
         return new DataLoader<K, V>(keys => Promise.all(keys.map(key => fetch(key))));
     }
 
@@ -17,39 +17,25 @@ export class FactomdDataLoader {
         return { load: () => fetch() };
     }
 
-    adminBlock = this.createDataloader<string | number, Factom.AdminBlock>(
-        this.cli.getAdminBlock.bind(this.cli)
-    );
+    adminBlock = this.createDataLoader(this.cli.getAdminBlock.bind(this.cli));
 
-    chainHead = this.createDataloader<string, Factom.EntryBlock>(
-        this.cli.getChainHead.bind(this.cli)
-    );
+    chainHead = this.createDataLoader(this.cli.getChainHead.bind(this.cli));
 
     currentMinute = this.createMockDataLoader(() =>
         this.cli.factomdApi('current-minute')
     );
 
-    directoryBlock = this.createDataloader<string | number, Factom.DirectoryBlock>(
-        this.cli.getDirectoryBlock.bind(this.cli)
-    );
+    directoryBlock = this.createDataLoader(this.cli.getDirectoryBlock.bind(this.cli));
 
     directoryBlockHead = this.createMockDataLoader(
         this.cli.getDirectoryBlockHead.bind(this.cli)
     );
 
-    entry = this.createDataloader<string, Factom.Entry>(
-        this.cli.getEntryWithBlockContext.bind(this.cli)
-    );
+    entry = this.createDataLoader(this.cli.getEntryWithBlockContext.bind(this.cli));
 
-    entryBlock = this.createDataloader<string, Factom.EntryBlock>(
-        this.cli.getEntryBlock.bind(this.cli)
-    );
+    entryBlock = this.createDataLoader(this.cli.getEntryBlock.bind(this.cli));
 
-    entryCreditBlock = this.createDataloader<string | number, Factom.EntryCreditBlock>(
-        this.cli.getEntryCreditBlock.bind(this.cli)
-    );
+    entryCreditBlock = this.createDataLoader(this.cli.getEntryCreditBlock.bind(this.cli));
 
-    factoidBlock = this.createDataloader<string | number, Factom.FactoidBlock>(
-        this.cli.getFactoidBlock.bind(this.cli)
-    );
+    factoidBlock = this.createDataLoader(this.cli.getFactoidBlock.bind(this.cli));
 }
