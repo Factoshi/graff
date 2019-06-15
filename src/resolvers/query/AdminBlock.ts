90b692c11777679fed5390e519e94d27442a9011
Alex <38507729+afenrir@users.noreply.github.com>
2019-06-15 22:03:38 +0100

    Remove getAdminBlockLeaves abstraction.

--- src/resolvers/query/AdminBlock.ts
+++ src/resolvers/query/AdminBlock.ts
@@ -2,16 +2,6 @@ import { AdminBlockResolvers, QueryResolvers } from '../../types/resolvers';
 import { FactomdDataLoader } from '../../data_loader';
 import { AdminBlock } from 'factom';
 
-export const getAdminBlockLeaves = async (
-    reference: string | number,
-    factomd: FactomdDataLoader
-) => {
-    return factomd.adminBlock
-        .load(reference)
-        .then(extractAdminBlockLeaves)
-        .catch(handleUnfound);
-};
-
 export const extractAdminBlockLeaves = (adminBlock: AdminBlock) => ({
     hash: adminBlock.backReferenceHash,
     height: adminBlock.directoryBlockHeight
@@ -29,11 +19,19 @@ export const handleUnfound = (error: Error) => {
  * Root Query resolvers that return a partial AdminBlock type.
  */
 export const adminBlockRootQueries: QueryResolvers = {
-    adminBlock: async (parent, { arg }, { factomd }) => getAdminBlockLeaves(arg, factomd),
+    adminBlock: (parent, { arg }, { factomd }) => {
+        return factomd.adminBlock
+            .load(arg)
+            .then(extractAdminBlockLeaves)
+            .catch(handleUnfound);
+    },
 
     adminBlockHead: async (root, args, { factomd }) => {
         const directoryBlockHead = await factomd.directoryBlockHead.load();
-        return getAdminBlockLeaves(directoryBlockHead.adminBlockRef, factomd);
+        return factomd.adminBlock
+            .load(directoryBlockHead.adminBlockRef)
+            .then(extractAdminBlockLeaves)
+            .catch(handleUnfound);
     }
 };
 
@@ -43,12 +41,18 @@ export const adminBlockRootQueries: QueryResolvers = {
 export const adminBlockResolvers: AdminBlockResolvers = {
     previousBlock: async (parent, args, { factomd }) => {
         const adminBlock = await factomd.adminBlock.load(parent.hash as string);
-        return getAdminBlockLeaves(adminBlock.previousBackReferenceHash, factomd);
+        return factomd.adminBlock
+            .load(adminBlock.previousBackReferenceHash)
+            .then(extractAdminBlockLeaves)
+            .catch(handleUnfound);
     },
 
     nextBlock: async (parent, args, { factomd }) => {
         const adminBlock = await factomd.adminBlock.load(parent.hash as string);
-        return getAdminBlockLeaves(adminBlock.directoryBlockHeight + 1, factomd);
+        return factomd.adminBlock
+            .load(adminBlock.directoryBlockHeight + 1)
+            .then(extractAdminBlockLeaves)
+            .catch(handleUnfound);
     },
 
     entries: async (parent, args, { factomd }) => {
