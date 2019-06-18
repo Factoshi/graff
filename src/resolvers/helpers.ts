dba05a75e50329b35f81e5bc5646fc2bede9dad2
Alex <38507729+afenrir@users.noreply.github.com>
2019-06-18 11:53:18 +0100

    Move helpers. Create tests.

--- src/resolvers/helpers.ts
+++
@@ -1,7 +0,0 @@
-export const handleBlockApiError = (error: Error) => {
-    if (error.message.endsWith('Block not found (code: -32008)')) {
-        return null;
-    } else {
-        throw error;
-    }
-};
