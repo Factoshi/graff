6eaca89d50ca347d86e8a1a6b071b7fb2205a968
Alex Carrithers <38507729+afenrir@users.noreply.github.com>
2019-07-03 11:43:33 +0100

    Export all resolvers.

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,12 +1,29 @@
 import { Query } from './Query';
-import { Hash, PublicEntryCreditAddress, PublicFactoidAddress } from './scalar';
+import { Hash, PublicEntryCreditAddress, PublicFactoidAddress, Height } from './scalar';
 import { Resolvers } from '../types/resolvers';
+import { adminBlockResolvers } from './AdminBlock';
+import { adminEntryResolvers } from './AdminEntry';
+import { directoryBlockResolvers } from './DirectoryBlock';
+import { entryBlockResolvers } from './EntryBlock';
+import { entryCreditBlockResolvers } from './EntryCreditBlock';
+import { factoidBlockResolvers } from './FactoidBlock';
+import { transactionResolvers } from './Transaction';
+import { entryResolvers } from './Entry';
 
 export const resolvers: Resolvers = {
     //Queries
+    AdminBlock: adminBlockResolvers,
+    AdminEntry: adminEntryResolvers,
+    DirectoryBlock: directoryBlockResolvers,
+    Entry: entryResolvers,
+    EntryBlock: entryBlockResolvers,
+    EntryCreditBlock: entryCreditBlockResolvers,
+    FactoidBlock: factoidBlockResolvers,
     Query,
+    Transaction: transactionResolvers,
     // Scalars
     Hash,
     PublicEntryCreditAddress,
-    PublicFactoidAddress
+    PublicFactoidAddress,
+    Height
 };
