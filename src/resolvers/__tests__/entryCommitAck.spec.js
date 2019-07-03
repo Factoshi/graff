734186bb1e2f477efaf08069c0c6478829bae9b9
Alex Carrithers <38507729+afenrir@users.noreply.github.com>
2019-07-03 11:34:34 +0100

    Fix ackRootQueries name.

--- src/resolvers/__tests__/entryCommitAck.spec.js
+++ src/resolvers/__tests__/entryCommitAck.spec.js
@@ -1,4 +1,4 @@
-const { ackRootQueries } = require('../EntryCommitAck');
+const { ackQueries } = require('../EntryCommitAck');
 const { FactomdDataLoader } = require('../../data_loader');
 const { cli } = require('../../factom');
 const { assert } = require('chai');
@@ -10,7 +10,7 @@ describe('EntryAck Resolvers', () => {
 
     it('Should get the leaves of EntryCommitAck from the commitAck resolver', async () => {
         const hash = '5f3599d372e9dfe9dd7fe5f2a72743496e99209b0a5776c1e34622fcb3c78e0b';
-        const ack = await ackRootQueries.commitAck(undefined, { hash }, { factomd });
+        const ack = await ackQueries.commitAck(undefined, { hash }, { factomd });
         assert.deepStrictEqual(ack, {
             entryHash: '6501bcfb818c5d24130c9ecd520071163a8692942f7fc11d48ea44e340c14904',
             commitHash: hash,
@@ -28,11 +28,7 @@ describe('EntryAck Resolvers', () => {
     it('Should get the leaves of EntryCommitAck from the entryAck resolver', async () => {
         const hash = '2346db420b35ee2c8e811305c36ea140ff47c59af70221be41ef3285e324d264';
         const chain = '9b5c6dbec96faef4f855182fa8d1475427eed27fc18f4c8deec588d1c252b7f8';
-        const ack = await ackRootQueries.entryAck(
-            undefined,
-            { hash, chain },
-            { factomd }
-        );
+        const ack = await ackQueries.entryAck(undefined, { hash, chain }, { factomd });
         assert.deepStrictEqual(ack, {
             entryHash: hash,
             commitHash:
