1d8f01c751ada6d281ed9883e771a6bd883c9f6e
Alex Carrithers <38507729+afenrir@users.noreply.github.com>
2019-07-03 11:32:02 +0100

    Make field resolvers responsible for leave.

--- src/resolvers/EntryBlock.ts
+++ src/resolvers/EntryBlock.ts
@@ -1,13 +1,5 @@
 import { EntryBlockResolvers, QueryResolvers, Entry } from '../types/resolvers';
-import { EntryBlock } from 'factom';
-import { handleBlockApiError, testPaginationInput } from './resolver-helpers';
-
-export const extractEntryBlockLeaves = (entryBlock: EntryBlock) => ({
-    hash: entryBlock.keyMR,
-    chain: entryBlock.chainId,
-    height: entryBlock.sequenceNumber,
-    timestamp: entryBlock.timestamp * 1000
-});
+import { testPaginationInput } from './resolver-helpers';
 
 /**
  * Root Query resolvers that return a partial EntryBlock type.
@@ -15,36 +7,23 @@ export const extractEntryBlockLeaves = (entryBlock: EntryBlock) => ({
 export const entryBlockQueries: QueryResolvers = {
     chainHead: async (root, { chain }, { factomd }) => {
         const chainHead = await factomd.chainHead.load(chain);
-        return factomd.entryBlock
-            .load(chainHead.keyMR)
-            .then(extractEntryBlockLeaves)
-            .catch(handleBlockApiError);
+        return { hash: chainHead.keyMR };
     },
 
-    entryBlock: async (root, { hash }, { factomd }) => {
-        return factomd.entryBlock
-            .load(hash)
-            .then(extractEntryBlockLeaves)
-            .catch(handleBlockApiError);
-    }
+    entryBlock: async (root, { hash }) => ({ hash })
 };
 
 /**
  * EntryBlock type resolvers.
  */
 export const entryBlockResolvers: EntryBlockResolvers = {
-    previousBlock: async (parent, args, { factomd }) => {
-        const entryBlock = await factomd.entryBlock.load(parent.hash as string);
-        return factomd.entryBlock
-            .load(entryBlock.previousBlockKeyMR)
-            .then(extractEntryBlockLeaves)
-            .catch(handleBlockApiError);
+    previousBlock: async ({ hash }, args, { factomd }) => {
+        const entryBlock = await factomd.entryBlock.load(hash!);
+        return { hash: entryBlock.previousBlockKeyMR };
     },
-
-    entries: async (parent, { offset = 0, first = Infinity }, { factomd }) => {
+    entries: async ({ hash }, { offset = 0, first = Infinity }, { factomd }) => {
         testPaginationInput(offset!, first!);
-        const entryBlock = await factomd.entryBlock.load(parent.hash as string);
-        const entryBlockLeaves = extractEntryBlockLeaves(entryBlock);
+        const entryBlock = await factomd.entryBlock.load(hash!);
         const entries = entryBlock.entryRefs
             .slice(offset!, offset! + first!)
             .map(({ entryHash, timestamp }) => ({
@@ -52,19 +31,37 @@ export const entryBlockResolvers: EntryBlockResolvers = {
                 chain: entryBlock.chainId,
                 // convert to milliseconds for consistency
                 timestamp: timestamp * 1000,
-                block: entryBlockLeaves
+                entryBlock: {
+                    hash: entryBlock.keyMR,
+                    chain: entryBlock.chainId,
+                    timestamp: entryBlock.timestamp,
+                    height: entryBlock.sequenceNumber
+                }
             })) as Entry[];
         return {
             entries,
             totalCount: entryBlock.entryRefs.length,
             offset: offset as number,
-            pageLength: entries.length,
-            finalPage: entries.length + offset! === entryBlock.entryRefs.length
+            pageLength: entries.length
         };
     },
-
-    directoryBlock: async (parent, args, { factomd }) => {
-        const entryBlock = await factomd.entryBlock.load(parent.hash as string);
-        return { height: entryBlock.directoryBlockHeight };
+    directoryBlock: async ({ hash }, args, { factomd }) => {
+        const entryBlock = await factomd.entryBlock.load(hash!);
+        const directoryBlock = await factomd.directoryBlock.load(
+            entryBlock.directoryBlockHeight
+        );
+        return { hash: directoryBlock.keyMR };
+    },
+    height: async ({ hash }, args, { factomd }) => {
+        const entryBlock = await factomd.entryBlock.load(hash!);
+        return entryBlock.sequenceNumber;
+    },
+    chain: async ({ hash }, args, { factomd }) => {
+        const entryBlock = await factomd.entryBlock.load(hash!);
+        return entryBlock.chainId;
+    },
+    timestamp: async ({ hash }, args, { factomd }) => {
+        const entryBlock = await factomd.entryBlock.load(hash!);
+        return entryBlock.timestamp;
     }
 };
--- src/resolvers/__tests__/entryBlock.spec.js
+++ src/resolvers/__tests__/entryBlock.spec.js
@@ -1,4 +1,4 @@
-const { entryBlockResolvers, entryBlockRootQueries } = require('../EntryBlock');
+const { entryBlockResolvers, entryBlockQueries } = require('../EntryBlock');
 const { FactomdDataLoader } = require('../../data_loader');
 const { cli } = require('../../factom');
 const { assert } = require('chai');
@@ -9,32 +9,23 @@ describe('EntryBlock Resolvers', () => {
 
     it('Should get the leaves of EntryBlock from the chainHead resolver', async () => {
         const chain = 'b47b83b04ba3b09305e7e02618c457c3fd82531a4ab81b16e73d780dfc2f3b18';
-        const chainHead = await entryBlockRootQueries.chainHead(
+        const chainHead = await entryBlockQueries.chainHead(
             undefined,
             { chain },
             { factomd }
         );
-        assert.hasAllKeys(chainHead, ['hash', 'chain', 'height', 'timestamp']);
+        assert.hasAllKeys(chainHead, ['hash']);
         assert.isString(chainHead.hash);
-        assert.isString(chainHead.chain);
-        assert.isNumber(chainHead.height);
-        assert.isNumber(chainHead.timestamp);
     });
 
     it('Should get the leaves of EntryBlock from the entryBlock resolver', async () => {
         const hash = '4dd4d88ab67c272817f78672768f5bac546743546c7755949f9c20a4583a0c9c';
-        const entryBlock = await entryBlockRootQueries.entryBlock(
+        const entryBlock = await entryBlockQueries.entryBlock(
             undefined,
             { hash },
             { factomd }
         );
         assert.strictEqual(entryBlock.hash, hash);
-        assert.strictEqual(
-            entryBlock.chain,
-            '4a35522c834022a4153c4ac92f61f22fad640647f91a21a65cf632f738717966'
-        );
-        assert.strictEqual(entryBlock.height, 1381);
-        assert.strictEqual(entryBlock.timestamp, 1560238680000);
     });
 
     it('Should get the previous EntryBlock', async () => {
@@ -45,31 +36,21 @@ describe('EntryBlock Resolvers', () => {
             { factomd }
         );
         assert.deepStrictEqual(previousBlock, {
-            hash: '82a609d7df91501b5669d1a9df34a6a3b7077aff8ee4308d4d00e03749e5a106',
-            chain: '9d8ae4cff16ebe2eeec8f2f73a2dab4ab6950521e5c3f26cfe5dd9bb80b7268d',
-            height: 0,
-            timestamp: 1560681180000
+            hash: '82a609d7df91501b5669d1a9df34a6a3b7077aff8ee4308d4d00e03749e5a106'
         });
     });
 
     it('should get all the entries in an entry block', async () => {
         const hash = 'fc056babef747ad84cce087c2bd446f0e93e0e848eff069dce08f623e4b3434f';
         const allEntries = await entryBlockResolvers.entries({ hash }, {}, { factomd });
-        assert.hasAllKeys(allEntries, [
-            'totalCount',
-            'entries',
-            'offset',
-            'pageLength',
-            'finalPage'
-        ]);
+        assert.hasAllKeys(allEntries, ['totalCount', 'entries', 'offset', 'pageLength']);
         assert.strictEqual(allEntries.totalCount, 41);
         assert.lengthOf(allEntries.entries, 41);
         assert.strictEqual(allEntries.offset, 0);
         assert.strictEqual(allEntries.pageLength, 41);
-        assert.isTrue(allEntries.finalPage);
         allEntries.entries.forEach(entry => {
-            assert.hasAllKeys(entry, ['hash', 'chain', 'timestamp', 'block']);
-            assert.hasAllKeys(entry.block, ['hash', 'chain', 'timestamp', 'height']);
+            assert.hasAllKeys(entry, ['hash', 'chain', 'timestamp', 'entryBlock']);
+            assert.hasAllKeys(entry.entryBlock, ['hash', 'chain', 'timestamp', 'height']);
             assert.isString(entry.hash);
             assert.isString(entry.chain);
             assert.isNumber(entry.timestamp);
@@ -83,18 +64,11 @@ describe('EntryBlock Resolvers', () => {
             { offset: 0, first: 20 },
             { factomd }
         );
-        assert.hasAllKeys(first20, [
-            'totalCount',
-            'entries',
-            'offset',
-            'pageLength',
-            'finalPage'
-        ]);
+        assert.hasAllKeys(first20, ['totalCount', 'entries', 'offset', 'pageLength']);
         assert.strictEqual(first20.totalCount, 41);
         assert.lengthOf(first20.entries, 20);
         assert.strictEqual(first20.offset, 0);
         assert.strictEqual(first20.pageLength, 20);
-        assert.isFalse(first20.finalPage);
         assert.strictEqual(
             first20.entries[0].hash,
             '8a39d74471c6ff2bf8ca38d4670577eec972f5e23335c327f02669d10b9a7e6a'
@@ -112,18 +86,11 @@ describe('EntryBlock Resolvers', () => {
             { offset: 21, first: 20 },
             { factomd }
         );
-        assert.hasAllKeys(last20, [
-            'totalCount',
-            'entries',
-            'offset',
-            'pageLength',
-            'finalPage'
-        ]);
+        assert.hasAllKeys(last20, ['totalCount', 'entries', 'offset', 'pageLength']);
         assert.strictEqual(last20.totalCount, 41);
         assert.lengthOf(last20.entries, 20);
         assert.strictEqual(last20.offset, 21);
         assert.strictEqual(last20.pageLength, 20);
-        assert.isTrue(last20.finalPage);
         assert.strictEqual(
             last20.entries[0].hash,
             '57561ab4ae34920e02629bf58bb59d4f301471ba9fcf0d8a9e24c8ec33b4d99e'
@@ -142,18 +109,11 @@ describe('EntryBlock Resolvers', () => {
             { offset: 30, first: 20 },
             { factomd }
         );
-        assert.hasAllKeys(entries, [
-            'totalCount',
-            'entries',
-            'offset',
-            'pageLength',
-            'finalPage'
-        ]);
+        assert.hasAllKeys(entries, ['totalCount', 'entries', 'offset', 'pageLength']);
         assert.strictEqual(entries.totalCount, 41);
         assert.lengthOf(entries.entries, 11);
         assert.strictEqual(entries.offset, 30);
         assert.strictEqual(entries.pageLength, 11);
-        assert.isTrue(entries.finalPage);
         assert.strictEqual(
             entries.entries[0].hash,
             '03fdffe3955ec618f6361cc8803fb3aa33abddf7e5499576e3f68be807547e07'
@@ -164,13 +124,15 @@ describe('EntryBlock Resolvers', () => {
         );
     });
 
-    it('Should get the directory block height', async () => {
+    it('Should get the directory block hash', async () => {
         const hash = '7a0f1b6ebb43bab079640e7ff192571b38b2b30cdfc7e50e9acfe1641499c2dc';
         const directoryBlock = await entryBlockResolvers.directoryBlock(
             { hash },
             {},
             { factomd }
         );
-        assert.deepStrictEqual(directoryBlock, { height: 197517 });
+        assert.deepStrictEqual(directoryBlock, {
+            hash: '20009134067fdac455c30b12548ab74bc4ffb6917d4936b73a55a42e66092111'
+        });
     });
 });
