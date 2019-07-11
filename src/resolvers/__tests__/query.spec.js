dff548c7f0606df80d51ab58df440176bd19d34b
Alex Carrithers <38507729+afenrir@users.noreply.github.com>
2019-07-11 16:52:01 +0100

    Move query helpers to query spec.

--- src/resolvers/__tests__/query.spec.js
+++ src/resolvers/__tests__/query.spec.js
@@ -2,10 +2,43 @@ const { query } = require('../Query');
 const { FactomdDataLoader } = require('../../data_loader');
 const { cli } = require('../../factom');
 const { assert } = require('chai');
-const {
-    generateMockPendingEntriesMethod,
-    generateMockPendingTransactionsMethod
-} = require('./testHelpers');
+const { randomBytes } = require('crypto');
+
+const generateMockPendingEntriesMethod = len => ({
+    factomd: {
+        pendingEntries: {
+            load: async () =>
+                Array(len)
+                    .fill(0)
+                    .map(() => ({
+                        entryhash: randomBytes(32).toString('hex'),
+                        chainid: randomBytes(32).toString('hex'),
+                        status: 'TransactionAck'
+                    }))
+        }
+    }
+});
+
+const generateMockPendingTransactionsMethod = len => ({
+    factomd: {
+        pendingTransactions: {
+            load: async () =>
+                Array(len)
+                    .fill(0)
+                    .map(() => ({
+                        hash: randomBytes(32).toString('hex'),
+                        status: 'TransactionAck',
+                        inputs: [],
+                        factoidOutputs: [],
+                        entryCreditOutputs: [],
+                        totalInputs: Math.floor(Math.random() * 10),
+                        totalFactoidOutputs: Math.floor(Math.random() * 10),
+                        totalEntryCreditOutputs: Math.floor(Math.random() * 10),
+                        fees: Math.floor(Math.random() * 10)
+                    }))
+        }
+    }
+});
 
 describe('Query Resolvers', () => {
     let factomd;
