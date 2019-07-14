2ebb2097fd27106f8df446d9a2a8287058ca5545
Alex Carrithers <38507729+afenrir@users.noreply.github.com>
2019-07-14 15:39:10 +0100

    Make broadcast methods abstract. Create add method

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -1,4 +1,5 @@
 import { MutationResolvers } from '../types/resolvers';
+import { FactomdDataLoader } from '../data_loader';
 
 enum BroadcastMethod {
     CommitChain = 'commit-chain',
@@ -8,36 +9,63 @@ enum BroadcastMethod {
     SubmitTx = 'factoid-submit'
 }
 
+const broadcastCommit = async (
+    factomd: FactomdDataLoader,
+    commit: string,
+    method: BroadcastMethod
+) =>
+    factomd.cli.factomdApi(method, { message: commit }).then(res => ({
+        entryHash: res.entryhash,
+        transactionHash: res.txid,
+        chainIdHash: res.chainidhash || null
+    }));
+
+const broadcastReveal = async (
+    factomd: FactomdDataLoader,
+    reveal: string,
+    method: BroadcastMethod
+) =>
+    factomd.cli.factomdApi(method, { entry: reveal }).then(res => ({
+        entryHash: res.entryhash,
+        chainId: res.chainid
+    }));
+
+const add = async (
+    factomd: FactomdDataLoader,
+    payload: { commit: string; reveal: string },
+    methods: { commit: BroadcastMethod; reveal: BroadcastMethod }
+) => {
+    const commitRes = await broadcastCommit(factomd, payload.commit, methods.commit);
+    const status = await factomd.cli.waitOnCommitAck(commitRes.transactionHash, 60);
+    if (status !== 'TransactionACK') {
+        throw new Error('Commit acknowledgement timed out.');
+    }
+    return broadcastReveal(factomd, payload.reveal, methods.reveal);
+};
+
 export const mutation: MutationResolvers = {
-    commitChain: async (_, { commit }, { factomd }) => {
-        const response = await factomd.cli.factomdApi(BroadcastMethod.CommitChain, {
-            message: commit
-        });
-        return {
-            entryHash: response.entryhash,
-            transactionHash: response.txid,
-            chainIdHash: response.chainidhash
-        };
+    commitChain: (_, { commit }, { factomd }) => {
+        return broadcastCommit(factomd, commit, BroadcastMethod.CommitChain);
     },
-    commitEntry: async (_, { commit }, { factomd }) => {
-        const response = await factomd.cli.factomdApi(BroadcastMethod.CommitEntry, {
-            message: commit
-        });
-        return {
-            entryHash: response.entryhash,
-            transactionHash: response.txid
-        };
+    commitEntry: (_, { commit }, { factomd }) => {
+        return broadcastCommit(factomd, commit, BroadcastMethod.CommitEntry);
     },
-    revealChain: async (_, { reveal }, { factomd }) => {
-        const response = await factomd.cli.factomdApi(BroadcastMethod.RevealChain, {
-            entry: reveal
-        });
-        return { entryHash: response.entryhash, chainId: response.chainid };
+    revealChain: (_, { reveal }, { factomd }) => {
+        return broadcastReveal(factomd, reveal, BroadcastMethod.RevealChain);
     },
-    revealEntry: async (_, { reveal }, { factomd }) => {
-        const response = await factomd.cli.factomdApi(BroadcastMethod.RevealEntry, {
-            entry: reveal
+    revealEntry: (_, { reveal }, { factomd }) => {
+        return broadcastReveal(factomd, reveal, BroadcastMethod.RevealEntry);
+    },
+    addChain: (_, args, { factomd }) => {
+        return add(factomd, args, {
+            commit: BroadcastMethod.CommitChain,
+            reveal: BroadcastMethod.RevealChain
         });
-        return { entryHash: response.entryhash, chainId: response.chainid };
+    },
+    addEntry: (_, args, { factomd }) => {
+        return add(factomd, args, {
+            commit: BroadcastMethod.CommitChain,
+            reveal: BroadcastMethod.RevealChain
+        });
     }
 };
