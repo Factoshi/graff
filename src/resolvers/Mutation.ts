603484f6bf85f5eec52844dd046908383d3a0865
Alex Carrithers <38507729+afenrir@users.noreply.github.com>
2019-07-14 13:33:04 +0100

    Create commitChain mutation resolver.

---
+++ src/resolvers/Mutation.ts
@@ -0,0 +1,22 @@
+import { MutationResolvers } from '../types/resolvers';
+
+enum BroadcastMethod {
+    CommitChain = 'commit-chain',
+    RevealChain = 'reveal-chain',
+    CommitEntry = 'commit-entry',
+    RevealEntry = 'reveal-entry',
+    SubmitTx = 'factoid-submit'
+}
+
+export const mutation: MutationResolvers = {
+    commitChain: async (_, { commit }, { factomd }) => {
+        const response = await factomd.cli.factomdApi(BroadcastMethod.CommitChain, {
+            message: commit
+        });
+        return {
+            entryHash: response.entryhash,
+            transactionHash: response.txid,
+            chainIdHash: response.chainidhash
+        };
+    }
+};
