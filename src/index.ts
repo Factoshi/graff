366d53f121328d7c1e610ae6fc8757bdbb9070e4
Alex Carrithers <38507729+afenrir@users.noreply.github.com>
2019-07-15 13:16:02 +0100

    Remove completed TODOs.

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,3 @@ server.listen().then(({ url }) => console.log(`Server ready at ${url} 🚀`));
 // TODO: figure out how to mitigate malicious queries
 // TODO: User config option for pagination limit
 // TODO: Review Docker config
-// TODO: add subscription unit tests
-// TODO: add mutations
-// TODO: add mutation unit tests
-// TODO: add mutation integration tests
