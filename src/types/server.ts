da88dd7be0c7596b004ab0f22cde2a84632c63e7
Alex Carrithers <38507729+afenrir@users.noreply.github.com>
2019-07-15 20:09:50 +0100

    Add caching dependencies.

--- src/types/server.ts
+++ src/types/server.ts
@@ -1,15 +1,10 @@
 import { FactomdDataLoader } from '../data_loader';
 import { PubSub } from 'apollo-server';
-import { DataSource } from 'apollo-datasource';
-import { FactomdDataSource } from '../datasource';
 import { Channel } from '../contants';
 
 export interface Context {
     factomd: FactomdDataLoader;
     pubsub: PubSub;
-    dataSources: {
-        factomd: FactomdDataSource;
-    };
 }
 
 export interface PublishParams {
